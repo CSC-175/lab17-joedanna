5c7cc3866017848926a03dad6580380fac5288f6
Joe <joedanna@umich.edu>
2024-11-20 09:54:49 -0500

    Attemp 1

--- lab17.cpp
+++ lab17.cpp
@@ -1,1 +1,12 @@
 // Definition of findHigh function goes here
+template <typename a, typename b, typename c,typename d>
+void findHigh(a Data[],b size,c &index,d &High) {
+
+
+        for(int i=0;i<size;i++) {
+                if(Data[i]>High) {
+                        High=Data[i];
+                        index=i;
+                }
+        }
+}
